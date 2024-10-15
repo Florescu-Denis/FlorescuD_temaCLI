f44cb90682422753110e9a8e74e78db0bd1c64f6
Florescu Denis <denistaur@gmail.com>
2024-10-15 22:10:32 +0300

    Tema2
    Mouse and keyboard controls

---
+++ Mouse/Mouse/Mouse/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace ConsoleApp1
+{
+    class SimpleWindow : GameWindow
+    {
+
+        private float rotationAngleX = 0.0f;
+        private float rotationAngleY = 0.0f;
+
+        public SimpleWindow() : base(800, 600)
+        {
+            MouseMove += Mouse_Move;
+        }
+        private void Mouse_Move(object sender, MouseMoveEventArgs e)
+        {
+            rotationAngleX = e.Y * 0.1f;
+            rotationAngleY = e.X * 0.1f;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.MidnightBlue);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Rotate(rotationAngleX, 1.0f, 0.0f, 0.0f);
+            GL.Rotate(rotationAngleY, 0.0f, 1.0f, 0.0f);
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(-1.0f, 1.0f);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(0.0f, -1.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(1.0f, 1.0f);
+            GL.End();
+            this.SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (SimpleWindow example = new SimpleWindow())
+            {
+                example.Run(30.0, 0.0);
+            }
+        }
+    }
+}
