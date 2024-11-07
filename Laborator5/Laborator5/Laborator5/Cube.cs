c06656663aeb32b91c73c90530fdf3cf068f2c8d
Florescu Denis <119164534+Lamayanul@users.noreply.github.com>
2024-11-07 21:45:06 +0200

    Laborator 5

---
+++ Laborator5/Laborator5/Laborator5/Cube.cs
@@ -0,0 +1,112 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK;
+using System;
+using System.Drawing;
+using System.IO;
+
+public class Cube
+{
+    private Vector3[] vertices;
+    private Color[] faceColors;
+    private Random random;
+
+    public Cube()
+    {
+        vertices = Vertex("coordonate.txt"); 
+        faceColors = new Color[6]; //array cu 6 culori pentru fiecare fata
+        random = new Random();
+        SetRandomColors(); // culori aleatorii fiecarei fete
+    }
+
+    private Vector3[] Vertex(string path)
+    {
+        Vector3[] loadedVertices = new Vector3[8]; //pt a stoca coordonatele varfurilor
+
+
+            string[] lines = File.ReadAllLines(path);
+            int index = 0;
+
+            foreach (string line in lines) // fiecare linie din fisier.
+        {
+
+                string[] coords = line.Split(',');
+                if (coords.Length == 3 && index < loadedVertices.Length)
+                {
+                    float x = float.Parse(coords[0].Trim());
+                    float y = float.Parse(coords[1].Trim());
+                    float z = float.Parse(coords[2].Trim());
+                    loadedVertices[index++] = new Vector3(x, y, z);
+                }
+            }
+        
+        
+
+        return loadedVertices;
+    }
+
+    public void SetFaceColor(int faceIndex, Color color)
+    {
+        if (faceIndex >= 0 && faceIndex < faceColors.Length) 
+            faceColors[faceIndex] = color;
+    }
+
+    public void SetRandomColors()  // setare culoare pt o fata
+    {
+        for (int i = 0; i < faceColors.Length; i++) // parcurge fiecare fata a cubului
+        {
+            faceColors[i] = Color.FromArgb(255, random.Next(256), random.Next(256), random.Next(256));
+        }
+    }
+
+    public void Draw()
+    {
+        GL.Begin(PrimitiveType.Quads);
+
+        for (int i = 0; i < faceColors.Length; i++)
+        {
+            GL.Color4(faceColors[i]);
+
+            switch (i)
+            {
+                case 0: // fata 1
+                    GL.Vertex3(vertices[0]);
+                    GL.Vertex3(vertices[1]);
+                    GL.Vertex3(vertices[2]);
+                    GL.Vertex3(vertices[3]);
+                    break;
+                case 1: // fata 2
+                    GL.Vertex3(vertices[4]);
+                    GL.Vertex3(vertices[5]);
+                    GL.Vertex3(vertices[6]);
+                    GL.Vertex3(vertices[7]);
+                    break;
+                case 2: // fata 3
+                    GL.Vertex3(vertices[0]);
+                    GL.Vertex3(vertices[1]);
+                    GL.Vertex3(vertices[5]);
+                    GL.Vertex3(vertices[4]);
+                    break;
+                case 3: // fata 4
+                    GL.Vertex3(vertices[2]);
+                    GL.Vertex3(vertices[3]);
+                    GL.Vertex3(vertices[7]);
+                    GL.Vertex3(vertices[6]);
+                    break;
+                case 4: // fata 5
+                    GL.Vertex3(vertices[0]);
+                    GL.Vertex3(vertices[3]);
+                    GL.Vertex3(vertices[7]);
+                    GL.Vertex3(vertices[4]);
+                    break;
+                case 5: // fata 6
+                    GL.Vertex3(vertices[1]);
+                    GL.Vertex3(vertices[2]);
+                    GL.Vertex3(vertices[6]);
+                    GL.Vertex3(vertices[5]);
+                    break;
+            }
+        }
+
+        GL.End();
+    }
+}
---
+++ Laborator5/Laborator5/Laborator5/Program.cs
@@ -0,0 +1,67 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Drawing;
+
+public class Window3D : GameWindow
+{
+    private Cube cube;
+
+    public Window3D() : base(800, 600)
+    {
+        VSync = VSyncMode.On;
+        cube = new Cube();
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+        base.OnLoad(e);
+        GL.ClearColor(Color.CornflowerBlue);
+        GL.Enable(EnableCap.DepthTest);
+        GL.DepthFunc(DepthFunction.Less);
+
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
+        GL.Viewport(0, 0, Width, Height);
+
+        Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)Width / (float)Height, 1, 1024);
+        GL.MatrixMode(MatrixMode.Projection);
+        GL.LoadMatrix(ref projection);
+
+        Matrix4 camera = Matrix4.LookAt(new Vector3(3, 3, 3), Vector3.Zero, Vector3.UnitY);
+        GL.MatrixMode(MatrixMode.Modelview);
+        GL.LoadMatrix(ref camera);
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        
+        KeyboardState input = Keyboard.GetState();
+        if (input.IsKeyDown(Key.C)) 
+        {
+            cube.SetRandomColors();
+        }
+        if (input.IsKeyDown(Key.Escape))
+        {
+            Exit();
+        }
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+        cube.Draw();
+
+        SwapBuffers();
+    }
+  
+}
